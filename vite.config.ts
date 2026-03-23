f109824578f8008973c5a57ce62ff0615c8879c2
Hamid Mayeli <Hamid.Mayeli@yahoo.com>
2026-03-23 14:54:38 +0000

    Clean up

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -12,7 +11,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
